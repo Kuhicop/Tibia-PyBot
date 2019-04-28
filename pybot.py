5c69a8f1ebf73589e66ae3dbfd305810c50d2674
Adrian Hernando <ahsolanas@gmail.com>
2019-04-28 20:46:27 +0200

    fixed menu & added fake console

--- pybot.py
+++ pybot.py
@@ -2,9 +2,52 @@ import pyautogui
 import os
 import sys
 import time
+from tkinter import *
+import threading
 import configparser
 
 running = True
+
+
+class FakeConsole(Frame):
+    def __init__(self, root, *args, **kargs):
+        Frame.__init__(self, root, *args, **kargs)
+
+        # white text on black background, for extra versimilitude
+        self.text = Text(self, bg="black", fg="white")
+        self.text.pack()
+
+        # list of things not yet printed
+        self.printQueue = []
+
+        # one thread will be adding to the print queue,
+        # and another will be iterating through it.
+        # better make sure one doesn't interfere with the other.
+        self.printQueueLock = threading.Lock()
+
+        self.after(5, self.on_idle)
+
+    # check for new messages every five milliseconds
+    def on_idle(self):
+        with self.printQueueLock:
+            for msg in self.printQueue:
+                self.text.insert(END, msg)
+                self.text.see(END)
+            self.printQueue = []
+        self.after(5, self.on_idle)
+
+    # print msg to the console
+    def show(self, msg, sep="\n"):
+        with self.printQueueLock:
+            self.printQueue.append(str(msg) + sep)
+
+def makeConsoles(amount):
+    root = Tk()
+    consoles = [FakeConsole(root) for n in range(amount)]
+    for c in consoles:
+        c.pack()
+    threading.Thread(target=root.mainloop).start()
+    return consoles
 
 
 def alerts():
@@ -17,8 +60,15 @@ def healer():
     menu()
 
 
+def runemaker_on():
+    print("Making runes.")
+    time.sleep(3)
+
+
 def runemaker():
     print("You've selected (3) Runemaker.")
+    thread1 = Thread(target=runemaker_on)
+    thread1.start()
     menu()
 
 
@@ -60,6 +110,14 @@ def menu():
         print("Wrong choice!")
         input("Press any key to exit...")
 
+
+# DEFAULT ROUTINE #
+
+a = makeConsoles(1)
+
+a.show("This is Console 1")
+a.show("I've got a lovely bunch of cocounts")
+a.show("Here they are standing in a row")
 
 # IMAGES
 # Move to images folder
