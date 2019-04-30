7e7d9e90ef7651b643708736fd625cc4a637525a
Adrian Hernando <ahsolanas@gmail.com>
2019-04-30 17:12:17 +0200

    updated

--- pybot.py
+++ pybot.py
@@ -1,53 +1,80 @@
-import pyautogui
 import os
-import sys
 import time
+import pyautogui
+import win32gui
+from threading import Thread
 from tkinter import *
-import threading
-import configparser
+from pynput import keyboard
 
-running = True
+bot = True
+running = False
+status_alerts = False
+status_healer = False
+status_runemaker = False
+spell = ""
+spell_time = 0
+
+# CREATE KEYS
+COMBINATION = [
+    {keyboard.Key.ctrl, keyboard.Key.end},
+    {keyboard.Key.shift, keyboard.Key.end}
+    ]
+
+current = set()
 
 
-class FakeConsole(Frame):
-    def __init__(self, root, *args, **kargs):
-        Frame.__init__(self, root, *args, **kargs)
+def execute():
+    print("executed2")
+    os._exit(1)
 
-        # white text on black background, for extra versimilitude
-        self.text = Text(self, bg="black", fg="white")
-        self.text.pack()
 
-        # list of things not yet printed
-        self.printQueue = []
+def on_press(key):
+    if any([key in COMBO for COMBO in COMBINATION]):
+        current.add(key)
+        if any(all(k in current for k in COMBO) for COMBO in COMBINATION):
+            execute()
 
-        # one thread will be adding to the print queue,
-        # and another will be iterating through it.
-        # better make sure one doesn't interfere with the other.
-        self.printQueueLock = threading.Lock()
 
-        self.after(5, self.on_idle)
+def on_release(key):
+    pass
 
-    # check for new messages every five milliseconds
-    def on_idle(self):
-        with self.printQueueLock:
-            for msg in self.printQueue:
-                self.text.insert(END, msg)
-                self.text.see(END)
-            self.printQueue = []
-        self.after(5, self.on_idle)
 
-    # print msg to the console
-    def show(self, msg, sep="\n"):
-        with self.printQueueLock:
-            self.printQueue.append(str(msg) + sep)
+def listen():
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
 
-def makeConsoles(amount):
-    root = Tk()
-    consoles = [FakeConsole(root) for n in range(amount)]
-    for c in consoles:
-        c.pack()
-    threading.Thread(target=root.mainloop).start()
-    return consoles
+
+threads = list()
+lt = Thread(target=listen)
+threads.append(lt)
+lt.start()
+
+
+class WindowMgr:
+    # FOCUS GAME
+    def __init__ (self):
+        self._handle = None
+
+    def find_window(self, class_name, window_name=None):
+        self._handle = win32gui.FindWindow(class_name, window_name)
+
+    def _window_enum_callback(self, hwnd, wildcard):
+        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
+            self._handle = hwnd
+
+    def find_window_wildcard(self, wildcard):
+        self._handle = None
+        win32gui.EnumWindows(self._window_enum_callback, wildcard)
+
+    def set_foreground(self):
+        win32gui.SetForegroundWindow(self._handle)
+
+
+def focus():
+    # FOCUS GAME
+    w = WindowMgr()
+    w.find_window_wildcard(".*Tibia.*")
+    w.set_foreground()
 
 
 def alerts():
@@ -60,15 +87,36 @@ def healer():
     menu()
 
 
-def runemaker_on():
-    print("Making runes.")
-    time.sleep(3)
+def eat_food():
+    try:
+        foodXY = pyautogui.locateOnScreen("food.png")
+        pyautogui.moveTo(foodXY.x, foodXY.y)
+        pyautogui.click(x=foodXY.x, y=foodXY.y, button="right")
+    except:
+        print("Exception: eat_food")
+
+def making_runes(rune_spell, rune_time):
+    while rune_spell != "":
+        try:
+            pyautogui.locateOnScreen("mana.png", grayscale=True)
+            # Make the rune
+            focus()
+            pyautogui.typewrite(rune_spell)
+            pyautogui.press('enter')
+            eat_food()
+        except:
+            print("Except: making runes")
+        print("Making runes: " + rune_spell)
+        time.sleep(rune_time)
 
 
 def runemaker():
     print("You've selected (3) Runemaker.")
-    thread1 = Thread(target=runemaker_on)
-    thread1.start()
+    print("")
+    global spell
+    spell = input("Spell: ")
+    global spell_time
+    spell_time = int(input("Spell time: "))
     menu()
 
 
@@ -79,20 +127,29 @@ def setup_client():
 
 def start():
     print("You've selected (0) START.")
-    menu()
+    global running
+    running = True
+    global spell
+    global spell_time
+    if spell != "":
+        thread1 = Thread(target=making_runes, args=(spell, spell_time, ))
+        thread1.start()
 
 
 # MENU
 def menu():
-    time.sleep(3)
     os.system('cls')
+
+    print("SHIFT + END WILL CLOSE THE BOT")
+    print("")
     print("(1) Alerts")
     print("(2) Healer")
     print("(3) Runemaker")
     print("")
     print("(5) Setup new client")
     print("")
-    print("(9) START")
+    print("(0) START")
+
     choice = input("Choice: ")
     os.system('cls')
 
@@ -112,13 +169,6 @@ def menu():
 
 
 # DEFAULT ROUTINE #
-
-a = makeConsoles(1)
-
-a.show("This is Console 1")
-a.show("I've got a lovely bunch of cocounts")
-a.show("Here they are standing in a row")
-
 # IMAGES
 # Move to images folder
 os.chdir("images")
@@ -132,5 +182,11 @@ for i in image_list:
         wait = input("Press any key to quit.")
         sys.exit(0)
 
-menu()
+
+while bot:
+    if running:
+        print("Bot running.")
+        time.sleep(1)
+    else:
+        menu()
 
